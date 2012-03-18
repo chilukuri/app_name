f3a0aa114c1a601af41ea239dbbe62ce5d3768c8
chilukuri <srikanth.chilukuri@accenture.com>
2012-03-18 09:40:32 +0530

    Done some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 AppName::Application.routes.draw do
-  get "pages/home"
+#  get "pages/home"
 
-  get "pages/contact"
-  get "pages/about"
+  #get "pages/contact"
+  #get "pages/about"
+  match '/home', :to => 'pages#home'
+    match '/', :to => 'pages#home'
 
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
